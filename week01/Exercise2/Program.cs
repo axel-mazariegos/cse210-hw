9067f23e7597dc0ed8d05d4a9f36c29729aaa5db
axel-mazariegos <amazariegoslicardie@byupathway.edu>
2025-09-05 22:43:14 -0600

    exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // 1. Get the user's grade and convert it to a number.
+        Console.Write("\nWhat is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        // 2. Determine the letter grade.
+        string letter = ""; // Create the letter variable
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // 2.1 Determine the sign grade.
+        string sign = ""; // Create the sign variable
+
+        if (percent >= 60 && percent <= 97) // Only assign signs for grades D, C, B, A-
+        {
+            int lastDigit = percent % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 2)
+            {
+                sign = "-";
+            }
+        }
+
+
+        // Print the letter grade after the if-chain is complete.
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // 3. Check if the user passed and display a message.
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!\n");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! You can do it next time.\n");
+        }
     }
 }
