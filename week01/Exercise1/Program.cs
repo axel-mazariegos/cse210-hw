bea6a02937868b8af65f70772ec68d434b9ae374
axel-mazariegos <amazariegoslicardie@byupathway.edu>
2025-09-05 22:31:10 -0600

    exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their first name.
+        Console.Write("What is your first name? ");
+        // Read the input and store it in a variable called "firstName".
+        string firstName = Console.ReadLine();
+
+        // Ask the user for their last name.
+        Console.Write("What is your last name? ");
+        // Read the input and store it in a variable called "lastName".
+        string lastName = Console.ReadLine();
+
+        // Use string interpolation ($) to create the final sentence and print it.
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
